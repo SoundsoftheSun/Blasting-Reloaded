032bd5800c2136de8c999418bf64c95cf19eb3f0
sun <sun@bluptr.xyz>
2025-12-20 02:13:38 -0500

    init

---
+++ src/main/java/net/soundsofthesun/blastingReloaded/BlastingReloaded.java
@@ -0,0 +1,10 @@
+package net.soundsofthesun.blastingReloaded;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BlastingReloaded implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/soundsofthesun/blastingReloaded/client/BlastingReloadedClient.java
@@ -0,0 +1,10 @@
+package net.soundsofthesun.blastingReloaded.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlastingReloadedClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
